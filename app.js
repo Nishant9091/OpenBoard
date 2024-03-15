280fe8b41fde12b0511e3b3784300cbff6c24489
Nishant Sharma <125345344+Nishant9091@users.noreply.github.com>
2024-03-15 15:05:56 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
-const socket = require("socket.io");
+const http = require("http");
+const socketIo = require("socket.io");
 
 const app = express();
 
@@ -18,12 +19,12 @@ app.use((req, res, next) => {
 
 app.use(express.static("public"));
 
-let port = process.env.PORT || 5000;
-let server = app.listen (port, () => {
-    console.log("Listening to port" + port);
-})
+const server = http.createServer(app);
+const io = socketIo(server);
 
-let io = socket(server);
+server.listen(5000, () => {
+    console.log("Server is running on port 5000");
+});
 
 io.on("connection", (socket) => {
     console.log("Made socket connection");
@@ -35,7 +36,8 @@ io.on("connection", (socket) => {
     socket.on("drawStroke", (data) => {
         io.sockets.emit("drawStroke", data);
     })
+
     socket.on("redoUndo", (data) => {
-        io.socket.emit("redoUndo", data);
+        io.sockets.emit("redoUndo", data);
     })
 })
